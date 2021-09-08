01d2e9d5f7d1da17ce8a5e92a240128e67e24e01
anchalDev12 <89698441+anchalDev12@users.noreply.github.com>
2021-09-08 14:39:02 +0530

    Create GaussJordan.m

---
+++ GaussJordan.m
@@ -0,0 +1,11 @@
+n = 6
+A = rand(n,n) # Random matrix_type
+I = eye(n,n) # Identify Matrix
+detA = det(A)  # Find Determinant 
+if(detA != 0), 
+  C = [A,I]     # Append Identity Matrix
+  reducedC = rref(C) # Find Rref of augmented matrix
+  invertedA = reducedC(:,7:2*n)  # Inverse(A) = invertedA
+else
+  disp("A is singular");
+endif
